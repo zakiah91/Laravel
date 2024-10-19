fe437d1378cc50c7bf9928a70b4e78c54249968e
zakiah91 <107566944+zakiah91@users.noreply.github.com>
2024-10-19 18:39:37 +0800

    Move the <modal> to here to simplify the route process

--- resources/views/layouts/defaultLayout.blade.php
+++ resources/views/layouts/defaultLayout.blade.php
@@ -21,6 +21,24 @@
 		
 		@yield('content')
 		
+		<div class="modal" tabindex="-1" id="postStatus" aria-labelledby="postStatusLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h5 class="modal-title">Status</h5>
+						<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+					</div>
+					<div class="modal-body" id="modalMsg">
+						<p id="modalText" style="display:block">Data is successfully inserted.</p>
+						<textarea id="myEditedContent" style="display:none;" class="form-control" rows="12" onKeyDown="checkTextEntered(event,'myEditedContent')"></textarea>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick="modalDone()">Done</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	
 		<br/><br/>
 		<div class="container">
 			<footer class="text-center">
