fb9b0b48675000d7216e6a6c008aa865e90806a7
zakiah91 <107566944+zakiah91@users.noreply.github.com>
2024-10-19 18:31:09 +0800

    Delete public/js/navbar.blade.php
    Delete due to wrong path

--- public/js/navbar.blade.php
+++
@@ -1,7 +0,0 @@
-@if(Session::has("isAllowAccess"))
-	<div class="navbar-nav">
-	   <a id="menu1" class="nav-link active" onclick="viewForm('main')" href="/main"><b>Write</b></a>
-	   <a id="menu2" class="nav-link" onclick="getContent()" href="/history"><b>History</b></a>
-	   <a id="menuLogout" class="nav-link" onClick="logout()" href="#"><b>Logout</b></a>
-	</div>
-@endif
