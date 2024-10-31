2d5fcedfbcb614dd66e903bf97f15b3df4946c9a
zakiah91 <107566944+zakiah91@users.noreply.github.com>
2024-10-31 15:34:48 +0800

    Add support for access to db using php
    add session for choosing server to run either using springboot or php
    add db access for login or forget password

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\DB;
 
 class PageController extends Controller{
 	
 	function __construct(){
 		
 	}
+	
 	
 	function setSession(){
 		Session::put("isAllowAccess","OK");
@@ -33,4 +36,42 @@ class PageController extends Controller{
 			return false;
 		}
 	}
+	
+	function setServer($selectedValue){
+		Session::put('webSettings.runServer',$selectedValue);
+	}
+	
+	function getServer(){
+		return Session::get('webSettings.runServer');
+	}
+	
+	function isServerSessionExist(){
+		return (Session::has('webSettings.runServer')) ?  true : false;
+	}
+	
+	function isLoginValid(Request $request){
+		
+		$tableName = "login";
+		
+		$db = DB::table($tableName)
+				->where("password",$request->post("password"))
+				->get();
+				
+		if(sizeof(json_decode(json_encode($db,true))) == 0)
+			return false;
+		else
+			return true;
+		
+	}
+	
+	function updateLogin(Request $request){
+		$tableName = "login";
+		
+		$db = DB::table($tableName)
+			  ->where("user_id",0)
+			  ->update(['password'=>$request->post('password')]);
+			  
+		return true;
+	}
+	
 }
