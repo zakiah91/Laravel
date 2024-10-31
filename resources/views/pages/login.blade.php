7961a99c6786ed72d89a3fab2074f1fc3da9d18c
zakiah91 <107566944+zakiah91@users.noreply.github.com>
2024-10-31 15:49:09 +0800

    Fix session judgement
    Add csrf token when post 
    Add judgement to check for serverSession when page have been ready

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,16 +2,21 @@
 
 @section('content')
 
+	<script type="text/javascript">
+		$(document).ready(()=>{setServerSelection('{{  $serverSession  }}')});
+	</script>
+
 	<div class="container" style="display:block;" id="login">
-		<div class="mt-5 row">
-			<label class="text-center"><b>Password : </b></label>
-			<input type="password" class="mt-3 form-control" id="pwd"/>
-		</div>
-		
-		<div class="mt-5 row">
-			<button type="button" class="btn btn-primary" onClick="tryLogin()">Submit</button>
-		</div>
-		
+			@csrf
+			<div class="mt-5 row">
+				<label class="text-center"><b>Password : </b></label>
+				<input type="password" class="mt-3 form-control" id="pwd"/>
+			</div>
+			
+			<div class="mt-5 row">
+				<button type="button" class="btn btn-primary" onClick="tryLogin()">Submit</button>
+			</div>
+
 		<div class="mt-5 row">
 			<a href="#" onClick="viewForm('login')">
 				<p class="text-center">Reset Password</p>
@@ -20,14 +25,15 @@
 	</div>
 	
 	<div class="container" style="display:none;" id="resetPwd">
-		<div class="mt-5 row">
-			<label class="text-center"><b>Your New Password : </b></label>
-			<input type="password" class="form-control" id="newPwd"/>	
-		</div>
-		
-		<div class="mt-5 row">
-			<button type="button" class="btn btn-primary" onCLick="reloadPage()">Confirm Your New Password</button>
-		</div>
+			<div class="mt-5 row">
+				<label class="text-center"><b>Your New Password : </b></label>
+				<input type="password" class="form-control" id="newPwd"/>	
+			</div>
+			
+			<div class="mt-5 row">
+				<button type="button" class="btn btn-primary" onCLick="reloadPage()">Confirm Your New Password</button>
+			</div>
 	</div>
 
+	
 @stop
